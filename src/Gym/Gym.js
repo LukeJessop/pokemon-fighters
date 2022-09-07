bc54196dca842ee3c05b5432f9d60dff161bfbf0
LukeJessop <jessop.lucas@gmail.com>
2022-09-07 17:02:41 -0600

    got access to api via axios, and now have some data displayed

--- src/Gym/Gym.js
+++ src/Gym/Gym.js
@@ -1,5 +1,13 @@
-function Gym(){
+import { useState } from 'react'
+import Pokemon from '../Pokemon/Pokemon'
 
+function Gym(props){
+return(
+    <div>
+        gym
+        <Pokemon pokemon={props}/>
+    </div>
+)
 }
 
 export default Gym
--- src/Hospital/Hospital.js
+++ src/Hospital/Hospital.js
@@ -1,5 +1,12 @@
-function Hospital(){
+import Pokemon from '../Pokemon/Pokemon'
 
+function Hospital(props){
+    return(
+        <div>
+            this is Hospital
+            <Pokemon pokemon={props}/>
+        </div>
+    )
 }
 
 export default Hospital
