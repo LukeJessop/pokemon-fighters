16e89cdc9071376d9521936009446e10823b1ac6
LukeJessop <jessop.lucas@gmail.com>
2022-09-19 17:11:25 -0600

    mainly just did some styling stuff for fighting component, i am trying to figure out how to have something that "ticks" every 10 seconds to display a new random pokemon. I found some stack overflow code and im going to spend tomorrow looking into it further

--- src/Components/Fighting/Fighting.js
+++ src/Components/Fighting/Fighting.js
@@ -1,49 +1,132 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Pokemon from '../Pokemon/Pokemon'
+import Pokemon from "../Pokemon/Pokemon";
+import FightingBackpack from "./FightingBackpack";
 
-function Fighting(){
+function Fighting() {
   const [pokeArr, setPokeArr] = useState([]); //array of custom stat pokemon from PokeAPI
-    useEffect(() => {
-        axios
-          .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1154")
-          .then( (res) => {
-            //get pokemon from pokeAPI
-            let customPokemonArr = []; //create new array to push the custom pokemon object to
-            let pokemonArr = res.data.results;
-            for (let i = 0; i < pokemonArr.length; i++) {
-              customPokemonArr.push({
-                id: i,
-                name: pokemonArr[i].name,
-                pokemonUrl: pokemonArr[i].url,
-                health: 100,
-                damage: 10,
-                xp: 0,
-                level: 1,
-                owner: 0,
-                inBackpack: false
-              });
-            }
-            console.log(customPokemonArr[1])
-            setPokeArr(customPokemonArr);
-          }).catch((err) => console.log('useEffect() in Fighting component ', err));
-      }, []);
 
-      const catchPokemon = (pokeId) => { //this function runs when a pokemon is clicked (TEMPORARY!! We will eventually replace the event trigger with when we win a fight with another pokemon) THIS NEEDS A POKEMON ID OR IT WILL NOT WORK
-        const { id, name, health, damage, level, pokemonUrl, xp, inBackpack } = pokeArr[pokeId];
-        axios.post("/api/pokemon", {id, name, health, damage, level, pokemonUrl, xp, inBackpack});
-      };
-  
-  
-  const pokemonMap = pokeArr.map((e, i) => <div key={i} onClick={() => catchPokemon(e.id)}><Pokemon pokemon={e} key={i}/></div>)
-    return(
-        <div>
-          <h1>Battle!</h1>
-            <div className="pokemon-list">
-              {pokemonMap}
-            </div>
+  const [enemyPokemon, setEnemyPokemon] = useState()
+
+  const [backpackOpen, setBackpackOpen] = useState(false);
+  const [isFighting, setIsFighting] = useState(false);
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1154") //get all pokemon and store them in pokeArr
+      .then((res) => {
+        //get pokemon from pokeAPI
+        let customPokemonArr = []; //create new array to push the custom pokemon object to
+        let pokemonArr = res.data.results;
+        for (let i = 0; i < pokemonArr.length; i++) {
+          customPokemonArr.push({
+            id: i,
+            name: pokemonArr[i].name,
+            pokemonUrl: pokemonArr[i].url,
+            health: 100,
+            damage: 10,
+            xp: 0,
+            level: 1,
+            owner: 0,
+            inBackpack: false,
+          });
+        }
+        setPokeArr(customPokemonArr);
+      })
+      .catch((err) => console.log("useEffect() in Fighting component ", err));
+
+      // setTimeout(() => {
+      //   let randomNum = Math.floor(Math.random() * 1154)
+      //   setEnemyPokemon([pokeArr[randomNum]])
+      // }, 10000)
+
+      const interval = setInterval(() => { //I FOUND THIS CODE ON STACK OVER FLOW INVESTIGATE FURTHER
+        let randomNum = Math.floor(Math.random() * 1154)
+        // setEnemyPokemon([pokeArr[randomNum]])
+        console.log(pokeArr[randomNum])
+      }, 1000);
+    
+      return () => clearInterval(interval);
+  }, []);
+
+
+  const catchPokemon = (pokeId) => {
+    //this function will run when you win a fight with another pokemon
+    const { id, name, health, damage, level, pokemonUrl, xp, inBackpack } = pokeArr[pokeId];
+    axios.post("/api/pokemon", {
+      id,
+      name,
+      health,
+      damage,
+      level,
+      pokemonUrl,
+      xp,
+      inBackpack,
+    });
+  };
+  return (
+    <>
+      <div className="fighting-graphic-container">
+        {isFighting ? (
+          <div>Fighting currently</div>
+        ) : (
+          <div>
+            {enemyPokemon}
+          </div>
+        )}
+      </div>
+
+      <div className="fighting-UI-container">
+        {backpackOpen ? (
+          <div className="fighting-container backpack">
+            <button onClick={() => setBackpackOpen(false)}>Back</button>
+            <FightingBackpack />
+          </div>
+        ) : (
+          <div className="fighting-container backpack">
+            <button
+              className="fighting-button"
+              onClick={() => setBackpackOpen(true)}
+            >
+              Backpack
+            </button>
+          </div>
+        )}
+
+        {isFighting ? (
+          <div className="fighting-container buttons">
+            <button className="fighting-button">Attack</button>
+            <button className="fighting-button">Heal</button>
+          </div>
+        ) : (
+          <div className="fighting-container buttons">
+            <button disabled className="fighting-button">
+              Attack
+            </button>
+            <button disabled className="fighting-button">
+              Heal
+            </button>
+          </div>
+        )}
+        <div className="fighting-container fight-or-flight">
+          {isFighting ? (
+            <button
+              className="fighting-button"
+              onClick={() => setIsFighting(false)}
+            >
+              Run Away
+            </button>
+          ) : (
+            <button
+              className="fighting-button"
+              onClick={() => setIsFighting(true)}
+            >
+              Attack
+            </button>
+          )}
         </div>
-    )
+      </div>
+    </>
+  );
 }
 
-export default Fighting
+export default Fighting;
--- src/Components/Fighting/FightingBackpack.js
+++ src/Components/Fighting/FightingBackpack.js
@@ -1,10 +1,26 @@
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function FightingBackpack() {
-
+    const [bpPokemon, setBpPokemon] = useState([])
+    useEffect(() => {
+        axios
+            .get('/api/backpack')
+            .then((res) => setBpPokemon(res.data))
+            .catch((err) => console.log(err.response.data))
+    }, [])
+    console.log(bpPokemon)
+    const backpackMap = bpPokemon.map((e) => {
+        return (
+            <div>
+                {e.name}
+                {e.level}
+            </div>
+        )
+    })
     return(
         <div>
-            
+            {backpackMap}
         </div>
     )
 }
