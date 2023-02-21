44021d3bde5133a9fa670796e9ebe5e869b15cfd
LukeJessop <70823991+LukeJessop@users.noreply.github.com>
2023-02-21 11:11:54 -0700

    ga

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,13 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Gym from "./Components/Gym/Gym";
-import Fighting from "./Components/Fighting/Fighting";
-import NoAuthPage from "./Components//Auth/NoAuthPage";
-import { useEffect } from "react";
-import ReactGA from 'react-ga'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Gym from './Components/Gym/Gym'
+import Fighting from './Components/Fighting/Fighting'
+import NoAuthPage from './Components//Auth/NoAuthPage'
 
-export default function ComponentRouter() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-  return (
+export default (
     <Routes>
-      <Route exact path="/" element={<NoAuthPage />} />
-      <Route path="/gym" element={<Gym />} />
-      <Route path="/fighting" element={<Fighting />} />
+        <Route exact path='/' element={<NoAuthPage />}/>
+        <Route path='/gym' element={<Gym />}/>
+        <Route path='/fighting' element={<Fighting />}/>
     </Routes>
-  );
-}
+)
