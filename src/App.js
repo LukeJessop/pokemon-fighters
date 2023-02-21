f32d9ebfb3676a6c92f38120e50fe9726f72aabd
LukeJessop <70823991+LukeJessop@users.noreply.github.com>
2023-02-21 11:09:13 -0700

    ga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Auth from "./Components/Auth/Auth";
 import { useLocation } from "react-router-dom";
-import componentRouter from "./routes";
+import ComponentRouter from "./routes";
 import Navbar from "./Components/Nav/Navbar";
 import ReactGA from "react-ga";
 import { useEffect } from "react";
@@ -9,16 +9,14 @@ ReactGA.initialize(TRACKING_ID);
 
 function App() {
   const location = useLocation();
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+  
 
   return (
     <div className="master-container font-link">
       <div className="side-nav-bar">
         {location.pathname === "/" ? <Auth /> : <Navbar />}
       </div>
-      <div className="page-container">{componentRouter}</div>
+      <div className="page-container">{ComponentRouter}</div>
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Gym from './Components/Gym/Gym'
-import Fighting from './Components/Fighting/Fighting'
-import NoAuthPage from './Components//Auth/NoAuthPage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Gym from "./Components/Gym/Gym";
+import Fighting from "./Components/Fighting/Fighting";
+import NoAuthPage from "./Components//Auth/NoAuthPage";
+import { useEffect } from "react";
+import ReactGA from 'react-ga'
 
-export default (
+export default function ComponentRouter() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+  return (
     <Routes>
-        <Route exact path='/' element={<NoAuthPage />}/>
-        <Route path='/gym' element={<Gym />}/>
-        <Route path='/fighting' element={<Fighting />}/>
+      <Route exact path="/" element={<NoAuthPage />} />
+      <Route path="/gym" element={<Gym />} />
+      <Route path="/fighting" element={<Fighting />} />
     </Routes>
-)
+  );
+}
