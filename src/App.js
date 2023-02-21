10886c107b42ef3004bd6e55082cc0200e606300
LukeJessop <70823991+LukeJessop@users.noreply.github.com>
2023-02-21 10:15:28 -0700

    ga

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-
 import Auth from "./Components/Auth/Auth";
-import {useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import componentRouter from "./routes";
 import Navbar from "./Components/Nav/Navbar";
-import ReactGA from 'react-ga'
-const TRACKING_ID = "UA-257529921-1"
-ReactGA.initialize(TRACKING_ID)
+import ReactGA from "react-ga";
+import { useEffect } from "react";
 
 function App() {
+  const TRACKING_ID = "UA-257529921-1";
+  ReactGA.initialize(TRACKING_ID);
   const location = useLocation();
-
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <div className="master-container font-link">
       <div className="side-nav-bar">
-        {location.pathname === '/' ? (<Auth/>) : (<Navbar/>)}
+        {location.pathname === "/" ? <Auth /> : <Navbar />}
       </div>
-      <div className="page-container">
-        {componentRouter}
-      </div>
+      <div className="page-container">{componentRouter}</div>
     </div>
   );
 }
