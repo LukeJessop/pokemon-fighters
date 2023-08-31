4438a8532ca39eb4a7447e0710da31b49f841423
LukeJessop <70823991+LukeJessop@users.noreply.github.com>
2023-08-31 12:50:02 -0600

    removed unused vars

--- src/Components/Fighting/Fighting.js
+++ src/Components/Fighting/Fighting.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Pokemon from "../Pokemon/Pokemon";
 import FightingBackpack from "./FightingBackpack";
